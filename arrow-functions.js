5e1ecefd9b86e51e33e711d0ba67df31e3de833d
Jari <orkolainenjari@gmail.com>
2025-08-20 15:54:05 +0300

    test3

---
+++ arrow-functions.js
@@ -0,0 +1,35 @@
+function sayHello() {
+    return "Hello, world!";
+}
+//const sayHelloArrow = () => "Hello, world!";
+
+function double(x) {
+    return x * 2;
+}
+//const doubleArrow = x => x * 2
+
+function add(x, y) {
+    return x + y;
+}
+//const addArrow = (x,y) => x + y;
+
+const person = {
+    name: "Alice",
+    sayHi: function() {
+        return "Hi, " + this.name + "!";
+    }
+};
+//const personArrow = {
+//  name: "Alice",
+//  sayHi: () => "Hi, " + this.name + "!" // 'this' will not work as expected here
+//};
+
+const numbers = [1, 2, 3, 4, 5];
+
+const doubled = [];
+numbers.forEach(function(num) {
+  doubled.push(num * 2);
+});
+//
+//
+//
